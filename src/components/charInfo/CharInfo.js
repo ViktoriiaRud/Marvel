fd883af78655caa1e41f6225ce543dd20df79c5e
Viktoriia <rudneva15v@gmail.com>
2022-10-12 17:16:52 +0200

    create ErrorBoundary

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -6,6 +6,8 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 
+import './charInfo.scss';
+
 class CharInfo extends Component {
 
     state = {
@@ -96,10 +98,15 @@ const View = ({char}) => {
 
     const {name, description, thumbnail, homepage, wiki, comics} = char;
 
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
     return (
         <>
              <div className="char__basics">
-                    <img src={thumbnail} alt={name}/>
+                    <img src={thumbnail} alt={name} style={imgStyle}/>
                     <div>
                         <div className="char__info-name">{name}</div>
                         <div className="char__btns">
@@ -117,8 +124,11 @@ const View = ({char}) => {
                 </div>
                 <div className="char__comics">Comics:</div>
                 <ul className="char__comics-list">
+
+                {comics.length > 0 ? null: 'There is not comics'}
                    {
                     comics.map((item, i) => {
+                        if(i > 10) return;
                       return (
                         <li key={i} className="char__comics-item">
                         {item.name}
